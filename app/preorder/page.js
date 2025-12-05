4438097882ab52f05c7b5fd2b1a80bcb253d030d
Jacob Ketola <h65k935@msu.montana.edu>
2025-12-05 12:00:03 -0700

    more changes

--- app/preorder/page.js
+++ app/preorder/page.js
@@ -7,13 +7,6 @@ import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { getAuth } from '../lib/authClient'
 
-export default function PreorderContent(){
-    return (
-        <Suspense>
-            <PreorderContent />
-        </Suspense>
-    );
-}
 
 export default function PreorderPage() {
     const params = useSearchParams()
